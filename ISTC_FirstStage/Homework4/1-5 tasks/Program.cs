940242b38171593528814949e7c570f257ac3ac3
David Mosyan <dmosyan@gmail.com>
2019-06-19 11:17:04 +0400

    Add files via upload

---
+++ ISTC_FirstStage/Homework4/1-5 tasks/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TASK 1: We have 2 positive numbers. Print "YES" if 1: Both of them are odd numbers 
+            // 2: One of them is odd number, 3: At least one og them is odd, 4: Both are odd numbers or both are even numbers 
+            uint A = UInt32.Parse(Console.ReadLine());
+            uint B = UInt32.Parse(Console.ReadLine());
+
+            // Option 1
+            if (A % 2 == 1 && B % 2 == 1) 
+            {
+                Console.WriteLine("YES");
+            } 
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            // Option 2
+            if (A % 2 == 1 || B % 2 == 1)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            // Option 3
+            if (A % 2 == 1 && B % 2 == 0 || A % 2 == 0 && B % 2 == 1)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 4
+            if (A % 2 == 1 && B % 2 == 1 || A % 2 == 0 && B % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //TASK 2: We have 3 integers (A,B and C). Print "YES" if 1: 3 of them are >0 2: At lesast one of the >0
+            //3: only one of them is > 0, 4: Both of them are > 0, 5: At least 2 of them are equal numbers, 6: At least 2 of them differ by sign.
+            int A = Int32.Parse(Console.ReadLine());
+            int B = Int32.Parse(Console.ReadLine());
+            int C = Int32.Parse(Console.ReadLine());
+
+            //Option 1
+            if (A>0 && B>0 && C>0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            //Option 2
+            if (A>0 || B>0 || C>0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 3
+            if ((A > 0 && B < 0 && C < 0
+                || A < 0 && B > 0 && C < 0
+                || A < 0 && B < 0 && C > 0
+                ))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 4
+            if ((A > 0 && B > 0 && C < 0
+                || A > 0 && B < 0 && C > 0
+                || A < 0 && B > 0 && C > 0
+                ))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            // Option 5
+            if ((A==B && C!=A)
+                || (A==C && B!=A)
+                || (B==C && B!= A))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 6
+            if ((Math.Abs(A) == Math.Abs(B) && Math.Abs(C) != Math.Abs(A))
+                || (Math.Abs(A) == Math.Abs(C) && Math.Abs(B) != Math.Abs(A))
+                || (B == C && B != A))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+             
+
+            //TASK 3: We have A > 0 number. If A is 2 digit number and even number, A is 3 digit number 
+            // and odd number print YES, else print NO
+            uint A = uint.Parse(Console.ReadLine());
+
+            if ((A>9 && A<100 &&  A%2==0)
+                || A>99 && A<1000 && A%2==1)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+            // TASK 4: We have A 3 digits integer. Print "YES" if 1: there are no equal digits, 2: every digit is smaller than the next one
+            // 3: if the number is polindrome, else print "NO"
+            int A = Int32.Parse(Console.ReadLine()); // for example 345
+            int thirdDigit = A % 10;                 // we should get 5
+            int newNumber = A / 10;                  // we should get 34
+            int secondDigit = A % 10;                // we should get 4               
+            int firstDigit = newNumber / 10;         // we should get 3 
+
+            //Option 1
+            if (firstDigit != secondDigit && firstDigit != thirdDigit && secondDigit != thirdDigit)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 2
+            if (firstDigit<secondDigit && secondDigit<thirdDigit)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 3
+            if (firstDigit<secondDigit && secondDigit<thirdDigit)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Option 4
+            if (firstDigit == thirdDigit)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+            //TASK 5: We have X and Y integers. Print "YES", if 1: Dot with (X,Y) coordinates is in second Quadrant
+            // 2: Dot with (X,Y) coordinates is in forth Quadrant, 3: Dot with (X,Y) coordinates is in second or third Quadrant
+            // 4: Dot with (X,Y) coordinates is in first or third Quadrant
+            
+            int X = Int32.Parse(Console.ReadLine());
+            int Y = Int32.Parse(Console.ReadLine());
+
+
+            //Option 1
+            if (X<0 && Y>0) 
+             {
+                Console.WriteLine("YES");
+             }
+            else
+            { 
+                Console.WriteLine("NO");
+            }
+
+            //Option 2
+            if (X>0 && Y<0) 
+             {
+                Console.WriteLine("YES");
+             }
+            else
+            { 
+                Console.WriteLine("NO");
+            }
+
+            //Option 3
+            if (X<0 && Y!=0) 
+             {
+                Console.WriteLine("YES");
+             }
+            else
+            { 
+                Console.WriteLine("NO");
+            }
+
+            //Option 4
+            if (X>0 && Y>0 || X<0 && Y<0 ) 
+             {
+                Console.WriteLine("YES");
+             }
+            else
+            { 
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
---
+++ ISTC_FirstStage/Homework4/6-10 tasks/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 6. We have x,y,x1,y1,x2,y2 > 0 integers. Print "YES" if dot with coordinates (x,y) is inside the 
+            // rectangulare which left upside corner has coordinates (x1,y1) and right bottom side corner has coordinates (x2,y2)
+            uint x = UInt32.Parse(Console.ReadLine());
+            uint y = UInt32.Parse(Console.ReadLine());
+            uint x1 = UInt32.Parse(Console.ReadLine());
+            uint y1 = UInt32.Parse(Console.ReadLine());
+            uint x2 = UInt32.Parse(Console.ReadLine());
+            uint y2 = UInt32.Parse(Console.ReadLine());
+
+            if (x1<x && y1>y && x2>x && y2<y)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //TASK 7: We have a,b,c natural numbers. Print "YES" if we can make triangle using numbers as sides length
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a + b > c || a + c > b || b + c > a)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //TASK 8 a,b,c are triangle sides. Print "YES" if 1: we can make equitable triangle
+            // 2: we can make triangle where 2 sides are equal to each other, 3: we can make triangle where one angle is 90 degree
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+
+            //Option 1
+            if (a == b && a==c)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            //Option 2
+            if (a == b && a != c || c==b && c!=a || a==c && c!=b)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            //Option 3
+            if ((Math.Pow(a,2) + Math.Pow(b,2) == Math.Pow(c, 2)) || (Math.Pow(a, 2) + Math.Pow(c, 2) == Math.Pow(b, 2))
+                || (Math.Pow(c, 2) + Math.Pow(b, 2) == Math.Pow(a, 2)))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //TASK 9: we have x,y numbers from 1 to 8 and we have chess board. Each number represents
+            // one coordinate and (x,y) gives us any cell in chess. Given x and y we should determine if
+            // the cell with that location is black or white.
+            int x = Int32.Parse(Console.ReadLine());
+            int y = Int32.Parse(Console.ReadLine());
+
+            if (x==1 && y==1 || x%2 == 1 && y%2 ==1 || x%2==0 && y%2==0)
+            {
+                Console.WriteLine("black");
+            }
+            else
+            {
+                Console.WriteLine("White");
+            }
+
+            //TASK 10:we have x1,y1,x2,y2 numbers from 1 to 8 and we have chess board. Each number represents
+            // one coordinate and (x,y) gives us any cell in chess. Determine if (x1,y1) cell has the same
+            // color as (x2,y2)
+            int x1 = Int32.Parse(Console.ReadLine());
+            int y1 = Int32.Parse(Console.ReadLine());
+            int x2 = Int32.Parse(Console.ReadLine());
+            int y2 = Int32.Parse(Console.ReadLine());
+
+            if ((x1+y1 %2 == 0 && x2+y2 %2 ==0) || (x1+y1 %2 != 0 && x2+y2 %2 != 0))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
