cc2ad63312c60e58a3ebe66d427a69413a4b7061
David Mosyan <dmosyan@gmail.com>
2019-08-19 13:49:26 +0400

    Add files via upload

---
+++ Third_stage/XML_JSON_(De)serialization/(De)Serialization_XML/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    [Serializable()]
+    public class Employee : ISerializable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public static int EmployeeID { get; set; }
+
+        public Employee() { }
+        
+        public Employee(string firstName, string lastName, string department, int employeeID)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+            EmployeeID = employeeID;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"firstName: {FirstName}, lastName: {LastName}, department: {Department}" +
+                $"ID: {EmployeeID}");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("firstName", FirstName);
+            info.AddValue("lastName", LastName);
+            info.AddValue("department", Department);
+            info.AddValue("ID", EmployeeID);
+        }
+
+        public Employee(SerializationInfo info, StreamingContext context)
+        {
+            FirstName = (string)info.GetValue("firstName", typeof(string));
+            LastName = (string)info.GetValue("lastName", typeof(string));
+            Department = (string)info.GetValue("department", typeof(string));
+            EmployeeID = (int)info.GetValue("ID", typeof(int));
+
+        }
+    }
+}
---
+++ Third_stage/XML_JSON_(De)serialization/(De)Serialization_XML/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee("Bob", "Johnson", "Marketing", 123456),
+                new Employee("Tom", "Smith", "Accounting", 654321),
+                new Employee("John", "Brown", "Development", 123654)
+            };
+            using (Stream xml = new FileStream(@"C:\Users\David\Desktop\Inheritance\Third_stage\XML_JSON_(De)serialization\Serialization\employee.xml",
+                FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>));
+                serializer.Serialize(xml, employees);
+            }
+            employees = null;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Employee>));
+            using (FileStream xml2 = File.OpenRead(@"C:\Users\David\Desktop\Inheritance\Third_stage\XML_JSON_(De)serialization\Serialization\employee.xml"))
+            {
+                employees = (List<Employee>)xmlSerializer.Deserialize(xml2);
+            }
+
+            foreach(Employee person in employees)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
+
