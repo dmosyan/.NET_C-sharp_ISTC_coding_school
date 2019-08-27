2f915e451ceeb683ebe62dd4e47c5855f11483ed
David Mosyan <dmosyan@gmail.com>
2019-08-27 22:48:04 +0400

    Add files via upload

---
+++ Third_stage/(Multi)Threading/Threading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        //Main Thread execution
+        static void Main(string[] args)
+        {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Main Thread";
+            Console.WriteLine($"This is {thread.Name}");
+        }
+    }
+}
