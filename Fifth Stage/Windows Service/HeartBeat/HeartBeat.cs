9013d49a8ca5040f8904cecf5cd092651454665a
David Mosyan <dmosyan@gmail.com>
2019-10-22 00:43:00 +0400

    Add files via upload

---
+++ Fifth Stage/Windows Service/HeartBeat/HeartBeat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Timers;
+using System.Diagnostics;
+
+namespace HeartBeat
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+
+        public HeartBeat()
+        {
+            _timer = new Timer(10000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Users\David\Desktop\Windows Services with .NET\HeartBeat\Log_File\HeartBeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ Fifth Stage/Windows Service/HeartBeat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Topshelf;
+
+namespace HeartBeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TopshelfExitCode exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new HeartBeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("HeartBeatService");
+                x.SetDisplayName("HeartBeat Service");
+                x.SetDescription("Test service");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
