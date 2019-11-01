5fb81472914fca8ba3a567849514c8ce433886ae
David Mosyan <dmosyan@gmail.com>
2019-11-01 23:12:32 +0400

    Add files via upload

---
+++ Fifth Stage/ASP.NET Core/ASP.NET Core2/CustomMiddleware/CustomMiddleWare.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleware
+{
+    public class CustomMiddleWare
+    {
+        // Delegate which will include the link for the next component of request
+        private readonly RequestDelegate next;
+
+        //Constructor
+        public CustomMiddleWare(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        //The name of the method should be Invoke or InvokeAsync
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //geting the type of the request
+            string method = context.Request.Method;
+            if (method == "GET")
+            {
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("That is GET request");
+            }
+            else
+                await next.Invoke(context);
+        }
+    }
+}
