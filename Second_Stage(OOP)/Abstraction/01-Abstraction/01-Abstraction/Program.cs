b3ad31b4396bcbb05b5b519840b57a4f2159a064
David Mosyan <dmosyan@gmail.com>
2020-04-06 14:18:57 +0400

    Add files via upload

---
+++ Second_Stage(OOP)/Abstraction/01-Abstraction/01-Abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Abstraction
+{
+    public abstract class AbstractClass
+    {
+        public abstract void Method();
+        //abstract method is th same as Virtual method
+    }
+
+    public class ConcreteClass : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Implementation");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass instance = new ConcreteClass();
+            instance.Method();
+            Console.ReadKey();
+        }
+    }
+}
