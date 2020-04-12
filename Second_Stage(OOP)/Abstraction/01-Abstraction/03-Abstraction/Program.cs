5aaf7b2a5f6d48e4597ebfc23b6092a46b9ed538
David Mosyan <dmosyan@gmail.com>
2020-04-13 00:08:07 +0400

    Add files via upload

---
+++ Second_Stage(OOP)/Abstraction/01-Abstraction/03-Abstraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Abstraction
+{
+    public abstract class AbstractClassA
+    {
+        public abstract void OperationA();
+    }
+    public abstract class AbstractClassB : AbstractClassA
+    {
+        public abstract void OperationB();
+    }
+    public class Concrete : AbstractClassA
+    {
+        public override void OperationA()
+        {
+            Console.WriteLine("AbstractClassA's Method Implementation");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClassA instance = new Concrete();
+            instance.OperationA();
+            //we cannot call instance.OperationB() Method
+            Console.ReadKey();
+        }
+    }
+}
