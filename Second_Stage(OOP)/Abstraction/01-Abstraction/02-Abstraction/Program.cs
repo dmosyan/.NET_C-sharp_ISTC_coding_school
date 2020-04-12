0ccd54769752efa82b513faf263c362048e19c23
David Mosyan <dmosyan@gmail.com>
2020-04-13 00:07:34 +0400

    Add files via upload

---
+++ Second_Stage(OOP)/Abstraction/01-Abstraction/02-Abstraction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Abstraction
+{
+    public class ClassA
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Method from ClassA");
+        }
+    }
+
+    public abstract class AbstractClass : ClassA
+    {
+        public abstract void Method();
+    }
+
+    public class ClassB : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Implementing abstract class method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass instance = new ClassB();
+            instance.Method();
+            instance.Operation();
+            Console.ReadKey();
+
+        }
+    }
+}
