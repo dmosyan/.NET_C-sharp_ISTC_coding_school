5937a08943fd9546a3054ee9f06c969f65582a53
David Mosyan <dmosyan@gmail.com>
2020-07-04 14:45:46 +0400

    Add files via upload

--- Projects/SchoolApp/SchoolLibrary/Student.cs
+++ Projects/SchoolApp/SchoolLibrary/Student.cs
@@ -8,5 +8,18 @@ namespace SchoolLibrary
     {
         public enum GradeLevels { Freshman, Sophomore, Junior, Senior }
         public GradeLevels GradeLevel { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            return 4.0f;
+        }
+
+        public override string SendMessage(string message)
+        {
+            String original = base.SendMessage(message);
+            StringBuilder sb = new StringBuilder(original);
+            sb.AppendLine("This message is confidential.");
+            return sb.ToString();
+        }
     }
 }
--- Projects/SchoolApp/SchoolLibrary/Teacher.cs
+++ Projects/SchoolApp/SchoolLibrary/Teacher.cs
@@ -4,8 +4,13 @@ using System.Text;
 
 namespace SchoolLibrary
 {
-    class Teacher : Person
+    public class Teacher : Person
     {
         public string Subject { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            return 0.0f;
+        }
     }
 }
